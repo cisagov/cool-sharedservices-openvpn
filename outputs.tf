9515ea1548f1043edea9ff1fabc94fb751461932
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-12-17 13:50:53 -0500

    Create maps instead of forcing the user to marry up multiple lists
    It is much simpler for the end user if we use a map instead of relying
    on him or her to marry up two lists containing the same number of
    items in a corresponding order.

--- outputs.tf
+++ outputs.tf
@@ -44,7 +44,7 @@ output "private_subnet_ids" {
 }
 
 output "private_subnet_nat_gw_ids" {
-  value       = aws_nat_gateway.nat_gws[*].id
+  value       = zipmap(module.private_subnets.subnet_ids, aws_nat_gateway.nat_gws[*].id)
   description = "The IDs corresponding to the NAT gateways used in the private subnets in the VPC"
 }
 
@@ -54,7 +54,7 @@ output "private_zone_id" {
 }
 
 output "private_subnet_private_reverse_zone_ids" {
-  value       = aws_route53_zone.private_subnet_private_reverse_zones[*].zone_id
+  value       = zipmap(module.private_subnets.subnet_ids, aws_route53_zone.private_subnet_private_reverse_zones[*].zone_id)
   description = "The zone IDs corresponding to the private Route53 reverse zones for the private subnets in the VPC"
 }
 
@@ -64,7 +64,7 @@ output "public_subnet_ids" {
 }
 
 output "public_subnet_private_reverse_zone_ids" {
-  value       = aws_route53_zone.public_subnet_private_reverse_zones[*].zone_id
+  value       = zipmap(module.public_subnets.subnet_ids, aws_route53_zone.public_subnet_private_reverse_zones[*].zone_id)
   description = "The zone IDs corresponding to the private Route53 reverse zones for the public subnets in the VPC"
 }
 
