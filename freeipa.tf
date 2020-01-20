3037aad9000da4e3576fe85f953c6372128a85c8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-20 13:43:30 -0500

    Make use of the variables added to variables.tf in the last commit

--- freeipa.tf
+++ freeipa.tf
@@ -17,15 +17,13 @@ module "ipa_master" {
   directory_service_pw        = var.ipa_directory_service_pw
   domain                      = var.cool_domain
   hostname                    = "ipa.${var.cool_domain}"
-  # TODO: At some point this should be a private subnet's reverse zone
-  private_reverse_zone_id = aws_route53_zone.public_subnet_private_reverse_zones[0].zone_id
-  private_zone_id         = aws_route53_zone.private_zone.zone_id
-  public_zone_id          = data.aws_route53_zone.public_zone.zone_id
-  realm                   = upper(var.cool_domain)
-  # TODO: At some point this should be a private subnet
-  subnet_id           = module.public_subnets.subnet_ids[0]
-  tags                = var.tags
-  trusted_cidr_blocks = var.trusted_cidr_blocks
+  private_reverse_zone_id     = var.master_private_reverse_zone_id
+  private_zone_id             = var.private_zone_id
+  public_zone_id              = data.aws_route53_zone.public_zone.zone_id
+  realm                       = upper(var.cool_domain)
+  subnet_id                   = var.master_subnet_id
+  tags                        = var.tags
+  trusted_cidr_blocks         = var.trusted_cidr_blocks
 }
 
 module "ipa_replica1" {
@@ -43,14 +41,12 @@ module "ipa_replica1" {
   cert_read_role_arn          = module.certreadrole_ipa_replica1.arn
   hostname                    = "ipa-replica1.${var.cool_domain}"
   master_hostname             = "ipa.${var.cool_domain}"
-  # TODO: At some point this should be a private subnet's reverse zone
-  private_reverse_zone_id  = aws_route53_zone.public_subnet_private_reverse_zones[1].zone_id
-  private_zone_id          = aws_route53_zone.private_zone.zone_id
-  public_zone_id           = data.aws_route53_zone.public_zone.zone_id
-  server_security_group_id = module.ipa_master.server_security_group_id
-  # TODO: At some point this should be a private subnet
-  subnet_id = module.public_subnets.subnet_ids[1]
-  tags      = var.tags
+  private_reverse_zone_id     = var.replica1_private_reverse_zone_id
+  private_zone_id             = var.private_zone_id
+  public_zone_id              = data.aws_route53_zone.public_zone.zone_id
+  server_security_group_id    = module.ipa_master.server_security_group_id
+  subnet_id                   = var.replica1_subnet_id
+  tags                        = var.tags
 }
 
 module "ipa_replica2" {
@@ -68,12 +64,10 @@ module "ipa_replica2" {
   cert_read_role_arn          = module.certreadrole_ipa_replica2.arn
   hostname                    = "ipa-replica2.${var.cool_domain}"
   master_hostname             = "ipa.${var.cool_domain}"
-  # TODO: At some point this should be a private subnet's reverse zone
-  private_reverse_zone_id  = aws_route53_zone.public_subnet_private_reverse_zones[2].zone_id
-  private_zone_id          = aws_route53_zone.private_zone.zone_id
-  public_zone_id           = data.aws_route53_zone.public_zone.zone_id
-  server_security_group_id = module.ipa_master.server_security_group_id
-  # TODO: At some point this should be a private subnet
-  subnet_id = module.public_subnets.subnet_ids[2]
-  tags      = var.tags
+  private_reverse_zone_id     = var.replica2_private_reverse_zone_id
+  private_zone_id             = var.private_zone_id
+  public_zone_id              = data.aws_route53_zone.public_zone.zone_id
+  server_security_group_id    = module.ipa_master.server_security_group_id
+  subnet_id                   = var.replica2_subnet_id
+  tags                        = var.tags
 }
