ee53445568832830f72b587d09ac4328e2e2716a
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-22 16:12:27 -0500

    Remove unused files

--- route53_zones.tf
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Create a data resource for the existing public Route53 zone.
-#-------------------------------------------------------------------------------
-data "aws_route53_zone" "public_zone" {
-  provider = aws.public_dns
-
-  name = var.public_zone_name
-}
