e5533b2aad46cd0de0bdee155ff3c7c2ea385872
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-01-24 14:34:04 -0500

    Use a profile instead of a role ARN to configure the backend
    Putting account IDs and such in a public repo is not a big deal, but
    some folks are already squeamish about using AWS so we may as well not
    give them anything extra to worry about.

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     encrypt        = true
     bucket         = "cisa-cool-terraform-state"
     dynamodb_table = "terraform-state-lock"
-    role_arn       = "arn:aws:iam::608004238745:role/TerraformStateAccess"
+    profile        = "terraform-role"
     region         = "us-east-1"
     key            = "cool-shared-services-openvpn/terraform.tfstate"
   }
