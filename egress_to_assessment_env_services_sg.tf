f7053b8bb0d979d96dcf4a01b85ea24407fe69d4
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-09-10 13:44:25 -0400

    Add an SG to allow VPN users access to services in assessment envs

---
+++ egress_to_assessment_env_services_sg.tf
@@ -0,0 +1,18 @@
+# Security group for assessment environment access
+resource "aws_security_group" "assessment_environment_services_access" {
+  vpc_id      = data.terraform_remote_state.networking.outputs.vpc.id
+  description = "Security group allowing access to assessment environment services"
+}
+
+# Access to assessment environment services (e.g., guacamole,
+# Mattermost, etc.)
+resource "aws_security_group_rule" "egress_to_assessment_env_services" {
+  for_each = local.assessment_env_service_ports
+
+  security_group_id = aws_security_group.assessment_environment_services_access.id
+  type              = "egress"
+  protocol          = each.value["protocol"]
+  cidr_blocks       = [data.terraform_remote_state.networking.outputs.cool_cidr_block]
+  from_port         = each.value["port"]
+  to_port           = each.value["port"]
+}
