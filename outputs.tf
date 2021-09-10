c943f65e4445185ab20a70a3f70b87e779923a84
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-09-10 13:50:42 -0400

    Add the new security group to the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "assessment_environment_services_access_security_group" {
+  value       = aws_security_group.assessment_environment_services_access
+  description = "The security group allowing VPN users access to services running in the assessment environments."
+}
+
 output "instance_id" {
   value       = module.openvpn.id
   description = "The ID corresponding to the OpenVPN server EC2 instance."
