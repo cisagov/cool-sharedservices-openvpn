3c221396b94e8c262e4c0ec8e3d610338f054506
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-09-17 22:07:50 -0400

    Add missing provider
    This was somehow left out in #42.  Mea culpa.

--- egress_to_assessment_env_services_sg.tf
+++ egress_to_assessment_env_services_sg.tf
@@ -1,5 +1,7 @@
 # Security group for assessment environment access
 resource "aws_security_group" "assessment_environment_services_access" {
+  provider = aws.provision_sharedservices
+
   vpc_id      = data.terraform_remote_state.networking.outputs.vpc.id
   description = "Security group allowing access to assessment environment services."
 }
@@ -7,6 +9,7 @@ resource "aws_security_group" "assessment_environment_services_access" {
 # Access to assessment environment services (e.g., Guacamole,
 # Mattermost, etc.)
 resource "aws_security_group_rule" "egress_to_assessment_env_services" {
+  provider = aws.provision_sharedservices
   for_each = local.assessment_env_service_ports
 
   security_group_id = aws_security_group.assessment_environment_services_access.id
