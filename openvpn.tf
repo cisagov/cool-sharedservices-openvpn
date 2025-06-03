ee99a5e2ae5b18b7ab6cae5f373be898dcfc5c9c
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-06-03 11:11:07 -0400

    Revert to using the default branch of cisagov/instance-cw-alarms-tf-module
    We can do this now that cisagov/instance-cw-alarms-tf-module#35 is
    merged.

--- openvpn.tf
+++ openvpn.tf
@@ -66,7 +66,7 @@ module "cw_alarms_openvpn" {
   providers = {
     aws = aws.provision_sharedservices
   }
-  source = "github.com/cisagov/instance-cw-alarms-tf-module?ref=improvement%2Fonly-handle-a-single-instance"
+  source = "github.com/cisagov/instance-cw-alarms-tf-module"
 
   alarm_actions             = [data.terraform_remote_state.sharedservices.outputs.cw_alarm_sns_topic.arn]
   instance_id               = module.openvpn.id
